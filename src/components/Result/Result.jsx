ed2e9651140eca22394c8d0ae5faba89c0cfd149
Bohdan Chelakov <chelakoff@gmail.com>
2023-04-09 02:21:47 +0300

    percent fix

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -14,7 +14,7 @@ const Result = () => {
   return (
     <section className="w-screen h-screen bg-gradient-to-r from-indigo-500 flex items-center justify-center">
       <div className="border border-black w-4/5 flex items-center justify-center flex-col backdrop-blur-lg bg-white bg-opacity-20 drop-shadow-lg p-6 rounded">
-        <p>{percent >= 23 ? `🫨🎉 🫵💋  ${percent}%` : `🤕 ${percent}% :((`}</p>
+        <p>{percent >= 64 ? `🫨🎉 🫵💋  ${percent}%` : `🤕 ${percent}% :((`}</p>
         <p>
           {correct}/{questions[0].length}
         </p>
